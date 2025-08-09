79ef82d67ab0450dc2017619eb272bb783a8afc9
masudrana2803 <masudrana2803@gmail.com>
2025-08-10 01:25:43 +0600

    first react

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const main = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default main
