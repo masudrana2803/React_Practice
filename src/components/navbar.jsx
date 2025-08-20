f52462ada5ad3aab52e091ab8f477b84c71e690a
masudrana2803 <masudrana2803@gmail.com>
2025-08-20 20:25:36 +0600

    working

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      <div className="navbar-row">
+        <div className="nav-log">
+            <img src="src/images/logo.png" alt="logo" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default navbar
